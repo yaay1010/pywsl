a106710b2f22bd9230f9c4a4a2f1530e2a370fef
yaay1010 <67664398+yaay1010@users.noreply.github.com>
2021-08-15 12:14:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import getuser
+import os
+username = getuser.lookup_username()
+
+def wsl():
+    while True:
+        command = input(r""+username+" |> ")
+        if command != "stop":
+            if command != "cls":
+                if command != "brr":
+                    if command != "ide":
+                        print(command)
+                    else:
+                        os.system('' if os.name == 'nt' else 'clear')
+                else:
+                    print("BRRRRRRRRRRRRRRRRR")
+                    print("BRRRRRRRRRRRRRRRRR")
+                    print("BRRRRRRRRRRRRRRRRR")
+                    print("BRRRRRRRRRRRRRRRRR")
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            exit(0)
+
+wsl()
